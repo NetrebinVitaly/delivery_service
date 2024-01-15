af48cfdca8bfe4ce6bd1f537a7afd92582393f8f
Burgoy <vit.netrebin@gmail.com>
2024-01-16 00:10:00 +0300

    Update migration

--- src/main/resources/migrations/V1__Init_DB.sql
+++ src/main/resources/migrations/V1__Init_DB.sql
@@ -1,25 +1,29 @@
-create table orders (
-    order_id bigserial not null,
-    order_address varchar(255) not null,
-    client_id bigint,
-    courier_id bigint,
-    description varchar(500),
-    order_status smallint check (order_status between 0 and 4), primary key (order_id)
+create table users
+(
+    login    varchar(50) not null,
+    email    varchar(50) not null,
+    password varchar(500),
+    role     varchar(255) check (role in ('ADMIN', 'COURIER', 'CLIENT')),
+    primary key (login)
 );
-create table users (
-    id bigserial not null,
-    email varchar(50) not null,
-    login varchar(50) not null,
-    password varchar(500) not null,
-    role varchar(255) check (role in ('ADMIN','COURIER','CLIENT')), primary key (id)
-);
-create index order_id_index on orders (order_id);
-create index client_id_index on orders (client_id);
-create index courier_id_index on orders (courier_id);
-create index user_id_index on users (id);
-create index user_login_index on users (login);
-create index user_email_index on users (email);
 
+create table orders
+(
+    order_id      bigserial    not null,
+    order_address varchar(255) not null,
+    description   varchar(500),
+    order_status  smallint     not null check (order_status between 0 and 4),
+    client        varchar(50),
+    courier       varchar(50),
+    primary key (order_id),
+    foreign key (client) references users,
+    foreign key (courier) references users
+);
+
+create index order_id_index on orders (order_id);
+create index client_index on orders (client);
+create index courier_index on orders (courier);
+create index user_login_index on users (login);
 -- alter table if exists users drop constraint if exists UK_6dotkott2kjsp8vw4d0m25fb7;
 
 insert into users(email, login, password, role)
